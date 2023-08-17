c35164510b0d5977ba33a549878f0bc7232f53db
Supriya Vatte <supriya.10724511@ltimindtree.com>
2023-08-17 10:51:34 +0530

    bug fixes-file upload

--- src/components/MostPopular.js
+++ src/components/MostPopular.js
@@ -6,12 +6,12 @@ const MostPopular = ({blogs}) => {
     const navigate =useNavigate(); 
   return (
     <div>
-        <div className='blog-heading text-start pt-3 py-2 mb-4'>Most Popular !!</div>
+        <div className='blog-heading text-start pt-3 py-2 mb-4'>Most Popular</div>
         {blogs?.map((item) =>(
             <div className='row pb-3' key={item?.id} style={{cursor:"pointer"}} onClick={()=>navigate(`/detail/${item?.id}`)}>
                 <div className='col-5 align-self-center'>
-                    {/* <img src ={item?.imgUrl} alt={item?.title} className="most-popular-img"/> */}
-                    <h4>{item?.title}</h4>
+                    <img src ={item?.imgUrl} alt={item?.title} className="most-popular-img"/>
+                    <h5>{item?.title}</h5>
                 </div>
                 <div className='col-7 padding'>
                     <div className='text-start most-popular-font'>{item?.title}</div>
